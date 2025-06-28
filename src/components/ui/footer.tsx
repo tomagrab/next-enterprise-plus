14c5c2b1500bcf777c580427b57d59ed85f61458
tomagrab <tomarrongrabowski@gmail.com>
2025-06-28 14:15:36 -0400

    fix: update footer to use fallback for company name in case of missing environment variable

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -3,7 +3,8 @@ export default function Footer() {
     <footer className="border-t border-border flex items-center justify-center py-2">
       <p className="text-xs text-muted-foreground">
         &copy; [Start Year] - {new Date().getFullYear()}{" "}
-        {process.env.NEXT_PUBLIC_COMPANY_NAME}. All rights reserved.
+        {process.env.NEXT_PUBLIC_COMPANY_NAME ?? "Company Name"}. All rights
+        reserved.
       </p>
     </footer>
   );
