eb0d07f955c61320a4de10a4887375966ac8906c
tomagrab <tomarrongrabowski@gmail.com>
2025-06-28 14:11:46 -0400

    fix: update footer to use environment variable for company name

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -2,8 +2,8 @@ export default function Footer() {
   return (
     <footer className="border-t border-border flex items-center justify-center py-2">
       <p className="text-xs text-muted-foreground">
-        &copy; [Start Year] - {new Date().getFullYear()} Company Name. All
-        rights reserved.
+        &copy; [Start Year] - {new Date().getFullYear()}{" "}
+        {process.env.NEXT_PUBLIC_COMPANY_NAME}. All rights reserved.
       </p>
     </footer>
   );
